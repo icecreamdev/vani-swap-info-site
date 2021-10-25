f5716a645f957354dd8349bcf91f6306bab4d79d
icedev <78191425+icecreamdev@users.noreply.github.com>
2021-10-24 21:23:05 -0300

    info price api

---
+++ precache-manifest.3297a473b8a0d6e1ff3c19ee2e5cc6d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fbf650f31c3c6a529549a8b575fe1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c8f425cd487d42b9b7",
+    "url": "/static/js/2.a44f2480.chunk.js"
+  },
+  {
+    "revision": "48dc2a0f18dfdbbcdcf81b94d8a3e1da",
+    "url": "/static/js/2.a44f2480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be4438782eafd8eec16",
+    "url": "/static/js/main.ce5bea32.chunk.js"
+  },
+  {
+    "revision": "d7b5075ecf70ba60e7e7",
+    "url": "/static/js/runtime-main.9cb2a799.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
